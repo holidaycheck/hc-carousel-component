fe10e1cd818beb2f536fe905127cb6374ad36411
wkriesing <wolfram.kriesing@holidaycheck.com>
2018-01-26 00:48:39 +0100

    Trying to style the buttons.

--- src/HcCarousel.js
+++ src/HcCarousel.js
@@ -1,5 +1,21 @@
+import { merge } from '../node_modules/elix/src/updates.js';
 import SlidingCarousel from '../node_modules/elix/src/SlidingCarousel.js';
 
-class HcCarousel extends SlidingCarousel {}
+class HcCarousel extends SlidingCarousel {
+
+  get updates() {
+    const base = super.updates || {};
+    return merge(base, {
+      $: {
+        arrowIconRight: {
+          style: {
+            color: 'green',
+          }
+        },
+      }
+    });
+  }
+
+}
 
 customElements.define('hc-carousel', HcCarousel);
