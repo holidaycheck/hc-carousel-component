de7ff04df66ab7ef35b23a2e91d39d3e204916b7
wolframkriesing <w+github@kriesing.de>
2018-01-31 21:38:03 +0100

    Trying to build my own `Thumb` ... but that failed :(.

--- src/ThumbsPreviewMixin.js
+++ src/ThumbsPreviewMixin.js
@@ -1,9 +1,36 @@
+import symbols from '../node_modules/elix/src/symbols.js';
+import { merge } from '../node_modules/elix/src/updates.js';
+
 import PageDotsMixin from '../node_modules/elix/src/PageDotsMixin.js';
+import ElementBase from '../node_modules/elix/src/ElementBase.js';
+
+
+class Thumb extends ElementBase {
+
+  get [symbols.template]() {
+    return `
+      <img src="">
+    `;
+  }
+  get updates() {
+    return merge(super.updates, {
+      $: {
+        pageDots: {
+          childNodes: this.pageDots
+        }
+      }
+    });
+  }
+}
 
 function HcThumbsPreviewMixin(Base) {
   // The class prototype added by the mixin.
   class HcThumbsPreview extends PageDotsMixin(Base) {
-
+    get pageDots() {
+      return [
+        //new Thumb()
+      ];
+    }
   }
   return HcThumbsPreview;
 }
