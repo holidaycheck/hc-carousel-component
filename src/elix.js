0fa4a629c6b4d3d093109f479dab7997cec3ff43
wolframkriesing <w+github@kriesing.de>
2018-02-01 23:52:50 +0100

    Fix the elix part.

--- src/elix.js
+++ src/elix.js
@@ -1,4 +1,4 @@
 // export * from '../node_modules/elix/src/elix.js';
 
-// https://github.com/elix/elix/blob/master/src/elix.js
-export * from '../../../../../elix/elix/blob/master/src/elix.js';
+// https://rawgit.com/elix/elix/master/src/elix.js
+export * from '../../../../../elix/elix/master/src/elix.js';
