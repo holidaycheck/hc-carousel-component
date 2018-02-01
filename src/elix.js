3a91cab49959d2eb894aa336c28223336f570958
wolframkriesing <w+github@kriesing.de>
2018-02-01 23:51:10 +0100

    Let's hope that this works on rawgit :).

--- src/elix.js
+++ src/elix.js
@@ -1,1 +1,4 @@
-export * from '../node_modules/elix/src/elix.js';
+// export * from '../node_modules/elix/src/elix.js';
+
+// https://github.com/elix/elix/blob/master/src/elix.js
+export * from '../../../../../elix/elix/blob/master/src/elix.js';
