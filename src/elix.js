93dca01304dd29f63fea6122eaf62aed0375bc47
wolframkriesing <wolfram+github@kriesing.de>
2018-02-05 21:25:20 +0100

    The customize branch is the new way, that Jan is experimenting with, which might become elix2.

--- src/elix.js
+++ src/elix.js
@@ -1,4 +1,4 @@
 // export * from '../node_modules/elix/src/elix.js';
 
 // https://rawgit.com/elix/elix/master/src/elix.js
-export * from '../../../../../elix/elix/master/src/elix.js';
+export * from '../../../../../elix/elix/customize/src/elix.js';
