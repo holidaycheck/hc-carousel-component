a975255288823488ab471e0e293b0b9a17d0311f
wolframkriesing <wolfram+github@kriesing.de>
2018-02-06 00:45:41 +0100

    A visual prototype of the thumb nails.

--- src/HcCarousel.js
+++ src/HcCarousel.js
@@ -6,17 +6,39 @@ const merge = elix.updates.merge;
 
 import '../src/HcArrowButton.js';
 
+const Base =
+  elix.ArrowDirectionMixin(
+  elix.CustomTagsMixin(
+    elix.SlidingPages
+  ));
+
 // Shows how a carousel subclass can define custom tags for the arrows and dots.
-class HcCarousel extends elix.SlidingCarousel {
+class HcCarousel extends Base {
   get defaultState() {
     return Object.assign({}, super.defaultState, {
       selectionWraps: true,
     });
   }
   get [elix.symbols.template]() {
-    return this[elix.ArrowDirectionMixin.inject](
-      super[elix.symbols.template]
-    );
+    return `
+      <style>
+        .thumb {
+          margin: .5rem .5rem 0 0;
+        }
+        .thumb:hover {
+          outline: 4px solid #fad73c;
+          outline-offset: -4px;
+        }
+      </style>
+      <div>
+        ${this[elix.ArrowDirectionMixin.inject](
+          super[elix.symbols.template]
+        )}
+        <img class="thumb" src="./1-thumb.jpg"/>
+        <img class="thumb" src="./2-thumb.jpg"/>
+        <img class="thumb" src="./3-thumb.jpg"/>
+      </div>
+    `;
   }
   get tags() {
     return Object.assign({}, super.tags, {
