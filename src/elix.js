66e885fa6f3e71a424ea70c703654c20c61901f5
wolframkriesing <wolfram+github@kriesing.de>
2018-02-06 20:39:12 +0100

    Use the elix v2 branch.

--- src/elix.js
+++ src/elix.js
@@ -1,4 +1,4 @@
 // export * from '../node_modules/elix/src/elix.js';
 
 // https://rawgit.com/elix/elix/master/src/elix.js
-export * from '../../../../../wolframkriesing/elix/customize-adjust/src/elix.js';
+export * from '../../../../../elix/elix/customize/src/elix.js';
