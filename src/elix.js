081e352f737bfcf669d769b9960bf3a586ca5b2b
wolframkriesing <wolfram+github@kriesing.de>
2018-02-06 20:40:59 +0100

    A bit of docs.

--- src/elix.js
+++ src/elix.js
@@ -1,4 +1,8 @@
+// When running locally you might want to use this
+// import.
 // export * from '../node_modules/elix/src/elix.js';
 
-// https://rawgit.com/elix/elix/master/src/elix.js
+// When pushing to github, enable the line below, so
+// the http://rawgit.com/... URL can use the elix source right from the repo.
+// No building necessary, and its always up to date.
 export * from '../../../../../elix/elix/customize/src/elix.js';
